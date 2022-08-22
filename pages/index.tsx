4db868385c341cf5c1fbbd76665ee9422b82acc5
jasham.qureshi <jasham.qureshi@covalensedigital.com>
2022-08-22 20:34:27 +0530

    updating tile

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Base Page Fackjscjksbkcbjksajkcb</div>
+  return <div className={styles.container}>Base Page Test</div>
 };
 
 export default Home;
